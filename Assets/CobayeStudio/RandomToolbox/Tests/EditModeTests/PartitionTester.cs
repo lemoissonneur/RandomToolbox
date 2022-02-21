6d0d63ee0e80abf39408d94bbafa5f1e8115257f
ArthurLEBARON <arthur.le-baron@i-teek.fr>
2022-02-21 12:28:39 +0100

    update tests

--- Assets/CobayeStudio/RandomToolbox/Tests/EditModeTests/PartitionTester.cs
+++ Assets/CobayeStudio/RandomToolbox/Tests/EditModeTests/PartitionTester.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.TestTools;
 
 
 namespace CobayeStudio.RandomToolbox
@@ -18,7 +17,7 @@ namespace CobayeStudio.RandomToolbox
                 0.5f, 0.003f, 0.075f, 0.09f, 0.12f, 0.0159f, 0.0137f, 0.035f, 0.237f, 0.0349f
             };
 
-            PartitionManagement.CorrectPartition(initialValues);
+            Partition.CorrectPartition(initialValues);
 
             Assert.AreEqual(1.00000f, initialValues.Sum());
         }
@@ -31,7 +30,7 @@ namespace CobayeStudio.RandomToolbox
                 0.3f, 0.003f, 0.075f, 0.09f, 0.12f, 0.0159f, 0.0137f, 0.035f, 0.237f, 0.0349f
             };
 
-            PartitionManagement.CorrectPartition(initialValues);
+            Partition.CorrectPartition(initialValues);
 
             Assert.AreEqual(1.00000f, initialValues.Sum());
         }
@@ -49,7 +48,7 @@ namespace CobayeStudio.RandomToolbox
                 0.3755f, 0.003f, 0.075f, 0.09f, 0.12f, 0.0159f, 0.0137f, 0.035f, 0.237f, 0.0349f
             };
 
-            PartitionManagement.CorrectPartition(initialValues);
+            Partition.CorrectPartition(initialValues);
 
             Assert.AreEqual(storedValues, initialValues);
         }
@@ -57,26 +56,26 @@ namespace CobayeStudio.RandomToolbox
         [Test]
         public void TestAddElement()
         {
-            PartitionBase partition = new Partition();
+            Partition partition = new Partition();
             Partition.Element newElementA = new Partition.Element() { Color = Color.red };
             Partition.Element newElementB = new Partition.Element() { Color = Color.blue };
 
-            partition.AddElement(newElementA);
-            partition.AddElement(newElementB);
+            partition.AddElement(newElementA, PartitionBase.CorrectionRule.None);
+            partition.AddElement(newElementB, PartitionBase.CorrectionRule.None);
 
             partition.SetValues(new float[2] { 0.2f, 0.8f });
             
-            Assert.AreEqual(Color.red, (partition as Partition).Elements[0].Color);
-            Assert.AreEqual(0.2f, (partition as Partition).Elements[0].Value);
+            Assert.AreEqual(Color.red, partition.Elements[0].Color);
+            Assert.AreEqual(0.2f, partition.Elements[0].Value);
 
-            Assert.AreEqual(Color.blue, (partition as Partition).Elements[1].Color);
-            Assert.AreEqual(0.8f, (partition as Partition).Elements[1].Value);
+            Assert.AreEqual(Color.blue, partition.Elements[1].Color);
+            Assert.AreEqual(0.8f, partition.Elements[1].Value);
         }
 
         [Test]
         public void TestAddElementGeneric()
         {
-            PartitionBase partition = new Partition<Vector3>();
+            Partition<Vector3> partition = new Partition<Vector3>();
             Partition<Vector3>.Element newElementA = new Partition<Vector3>.Element() { Color = Color.red, Object = Vector3.up };
             Partition<Vector3>.Element newElementB = new Partition<Vector3>.Element() { Color = Color.blue, Object = Vector3.down };
 
@@ -85,13 +84,13 @@ namespace CobayeStudio.RandomToolbox
 
             partition.SetValues(new float[2] { 0.2f, 0.8f });
 
-            Assert.AreEqual(Color.red, (partition as Partition<Vector3>).Elements[0].Color);
-            Assert.AreEqual(0.2f, (partition as Partition<Vector3>).Elements[0].Value);
-            Assert.AreEqual(Vector3.up, (partition as Partition<Vector3>).Elements[0].Object);
+            Assert.AreEqual(Color.red, partition.Elements[0].Color);
+            Assert.AreEqual(0.2f, partition.Elements[0].Value);
+            Assert.AreEqual(Vector3.up, partition.Elements[0].Object);
 
-            Assert.AreEqual(Color.blue, (partition as Partition<Vector3>).Elements[1].Color);
-            Assert.AreEqual(0.8f, (partition as Partition<Vector3>).Elements[1].Value);
-            Assert.AreEqual(Vector3.down, (partition as Partition<Vector3>).Elements[1].Object);
+            Assert.AreEqual(Color.blue, partition.Elements[1].Color);
+            Assert.AreEqual(0.8f, partition.Elements[1].Value);
+            Assert.AreEqual(Vector3.down, partition.Elements[1].Object);
         }
     }
 }
